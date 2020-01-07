176a8165eb2b223e9f7aede5e23c8540ecd40e8c
fegzycole <fergusoniyara@gmail.com>
2020-01-07 21:36:25 +0100

    Fix rubocop issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 200 }
   belongs_to :user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 5..20 }
   validates :body, presence: true, length: { maximum: 200 }
